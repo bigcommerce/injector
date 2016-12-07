88948fb601f12284d841d5331773465252536bbc
Adam Benson <adam.benson@bigcommerce.com>
2016-12-07 17:05:45 +1100

    Winning all the scruitinizer points.

--- src/Reflection/ParameterInspector.php
+++ src/Reflection/ParameterInspector.php
@@ -32,7 +32,7 @@ class ParameterInspector
      * Fetch the method signature of a method when we have already created a \ReflectionClass
      * @param \ReflectionClass $reflectionClass
      * @param string $methodName
-     * @return array|null
+     * @return array
      * @throws \ReflectionException
      */
     public function getSignatureByReflectionClass(\ReflectionClass $reflectionClass, $methodName)
@@ -45,7 +45,7 @@ class ParameterInspector
      * Fetch the method signature of a method using its fully qualified class name, and method name.
      * @param string $className
      * @param string $methodName
-     * @return array|null
+     * @return array
      * @throws \ReflectionException
      */
     public function getSignatureByClassName($className, $methodName)
@@ -61,7 +61,7 @@ class ParameterInspector
      * @param string $className Fully qualified class name
      * @param string $methodName Name of the method we're inspecting
      * @param \ReflectionClass $refClass Optional existing ReflectionClass for this class
-     * @return array|null The signature of this methods parameters as an array.
+     * @return array The signature of this methods parameters as an array.
      * @throws \ReflectionException
      */
     private function getMethodSignature($className, $methodName, \ReflectionClass $refClass = null)
