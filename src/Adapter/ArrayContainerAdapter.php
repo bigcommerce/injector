4da9f62f94c364af9d10bccc6a7e4dde23a1e4b8
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-01-14 10:33:54 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Adapter/ArrayContainerAdapter.php
+++ src/Adapter/ArrayContainerAdapter.php
@@ -35,7 +35,7 @@ class ArrayContainerAdapter implements ContainerInterface
      */
     public function get($id)
     {
-        if(!$this->has($id)){
+        if (!$this->has($id)) {
             throw new ServiceNotFoundException("Service not found in container ($id).");
         }
         return $this->arrayContainer[$id];
